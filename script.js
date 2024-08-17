1ba71318575245bca52f9a593aafb0c8ec0103d0
Dev-Wasp <osadchyi.maksym.02@gmail.com>
2024-08-17 20:04:55 +0300

    Add functionality to play five games in a row
    Add playGame function and rearrange the code to
    allow users to play five games in a row.

--- script.js
+++ script.js
@@ -1,10 +1,61 @@
-let humanScore = 0;
-let computerScore = 0;
+playGame();
 
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-playRound(humanChoice, computerChoice);
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+
+  if (humanScore > computerScore) {
+    console.log(`You win the game with the score of ${humanScore}:${computerScore}`);
+  }
+  else if (humanScore < computerScore) {
+    console.log(`You lost the game. The score is ${humanScore}:${computerScore}`);
+  }
+  else {
+    console.log(`It is a draw. The score is ${humanScore}:${computerScore}`);
+  }
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log("It's a tie!");
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log("You win! Rock beats scissors.");
+
+      humanScore++;
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+      console.log("You lose! Paper beats rock.");
+
+      computerScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log("You win! Paper beats rock.");
+
+      humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+      console.log("You lose! Scissors beat paper.");
+
+      computerScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log("You win! Scissors beat paper.");
+
+      humanScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+      console.log("You lose! Rock beats scissors.");
+
+      computerScore++;
+    }
+  }
+}
 
 function getComputerChoice() {
   let computerChoice;
@@ -28,40 +79,4 @@ function getHumanChoice() {
   const humanChoice = prompt("Enter your choice (\"rock\", \"paper\", or \"scissors\": ");
 
   return humanChoice.toLowerCase();
-}
-
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log("It's a tie!");
-  }
-  else if (humanChoice === "rock" && computerChoice === "scissors") {
-    console.log("You win! Rock beats scissors.");
-
-    humanScore++;
-  }
-  else if (humanChoice === "rock" && computerChoice === "paper") {
-    console.log("You lose! Paper beats rock.");
-
-    computerScore++;
-  }
-  else if (humanChoice === "paper" && computerChoice === "rock") {
-    console.log("You win! Paper beats rock.");
-
-    humanScore++;
-  }
-  else if (humanChoice === "paper" && computerChoice === "scissors") {
-    console.log("You lose! Scissors beat paper.");
-
-    computerScore++;
-  }
-  else if (humanChoice === "scissors" && computerChoice === "paper") {
-    console.log("You win! Scissors beat paper.");
-
-    humanScore++;
-  }
-  else if (humanChoice === "scissors" && computerChoice === "rock") {
-    console.log("You lose! Rock beats scissors.");
-
-    computerScore++;
-  }
 }
