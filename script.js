873969ea6aab1a28cab701af4ceb82219671db7b
Dev-Wasp <osadchyi.maksym.02@gmail.com>
2024-08-17 19:45:20 +0300

    Add playRound function to run a round of the game

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let humanScore = 0;
 let computerScore = 0;
 
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
+
 function getComputerChoice() {
   let computerChoice;
 
@@ -23,4 +28,40 @@ function getHumanChoice() {
   const humanChoice = prompt("Enter your choice (\"rock\", \"paper\", or \"scissors\": ");
 
   return humanChoice.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  }
+  else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats scissors.");
+
+    humanScore++;
+  }
+  else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats rock.");
+
+    computerScore++;
+  }
+  else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats rock.");
+
+    humanScore++;
+  }
+  else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beat paper.");
+
+    computerScore++;
+  }
+  else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win! Scissors beat paper.");
+
+    humanScore++;
+  }
+  else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats scissors.");
+
+    computerScore++;
+  }
 }
